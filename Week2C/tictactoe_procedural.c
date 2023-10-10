fa1f25d7f718f0096cdb446fb5174d89fcec769a
Richard Austin <richardaustin123@icloud.com>
2023-10-10 17:17:21 +0100

    Extra comments

--- Week2C/tictactoe_procedural.c
+++ Week2C/tictactoe_procedural.c
@@ -14,8 +14,7 @@ char board[9] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
 
 // displayBoard()
 // Display the board in ASCII
-void displayBoard()
-{
+void displayBoard() {
     printf("-------\n");
     printf("|%c|%c|%c|\n",board[0],board[1],board[2]);
     printf("|%c|%c|%c|\n",board[3],board[4],board[5]);
@@ -25,10 +24,10 @@ void displayBoard()
 
 // int winner(+player)
 // Check if the player has won
-int winner(char player)
-{
+int winner(char player) {
     for(int i = 0; i < 8; i++)
-        if(board[winningCombos[i][0]] == player && 
+        // If the player has their , return 1
+        if(board[winningCombos[i][0]] == player &&  
            board[winningCombos[i][1]] == player && 
            board[winningCombos[i][2]] == player)
             return 1;
@@ -37,30 +36,26 @@ int winner(char player)
 
 // takeTurn(+player)
 // Ask the player for a move and update the board
-void takeTurn(char player)
-{
+void takeTurn(char player) {
     printf("it's player %c's turn\n", player);
     printf("Enter move index (1-9): ");
     char * input[10];
-    fgets(input,10,stdin);
-    int move = atoi(input);
-    board[move - 1] = player;
+    fgets(input,10,stdin);      // Read from standard input
+    int move = atoi(input);     // Convert string to integer
+    board[move - 1] = player;   // move-1 as the board is 0-8 but user types 1-9
     printf("\n");
 }
 
 // tictactoe()
 // Call the whole game       
-void tictactoe()
-{
-    char players[2] = {'O','X'};
+void tictactoe() {
+    char players[2] = {'O','X'};            // Player 1 is O, Player 2 is X
     int playerIndex = 0;
-    for(int i = 0; i < 9; i++)
-    {
-        char player = players[playerIndex];
+    for(int i = 0; i < 9; i++) {
+        char player = players[playerIndex]; // Get the current player
         displayBoard();
         takeTurn(player);
-        if(winner(player))
-        {
+        if(winner(player)) {
             displayBoard();
             printf("Player %c wins!", player);
             break;
